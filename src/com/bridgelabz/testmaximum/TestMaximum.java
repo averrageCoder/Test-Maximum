aa02e961e35236b7b2855554b6d62dbf26052c59
Harsh R Jain <rjainharsh@gmail.com>
2021-08-29 14:07:59 +0530

    REFACTORED: one generic method to find maximum of all 3 types

--- src/com/bridgelabz/testmaximum/TestMaximum.java
+++ src/com/bridgelabz/testmaximum/TestMaximum.java
@@ -2,19 +2,32 @@ package com.bridgelabz.testmaximum;
 
 public class TestMaximum {
 
+	public static <E extends Comparable<E>> void findMaximum(E element1, E element2, E element3) {
+		E maximum = element1;
+		
+		if(element2.compareTo(maximum) > 0)
+			maximum=element2;
+		
+		if(element3.compareTo(maximum) > 0)
+			maximum=element3;
+		
+		printMax(element1, element2, element3, maximum);
+	}
+	
+	public static <E> void printMax(E element1,E element2,E element3, E maximum) {
+		System.out.println("Maximum of "+element1+", "+element2+" & "+element3+" is: "+maximum);
+	}
+	
 	public static void main(String[] args) {
 		
-		String string1="adamX", string2="adam1", string3="adam2", maximum;
-		maximum = string1;
+		Integer integer1=-10, integer2=-3, integer3=-1;
+		findMaximum(integer1,integer2,integer3);
 		
-		if(string2.compareTo(maximum) > 0)
-			maximum=string2;
+		Float number1=10f, number2=-3f, number3=-1f;
+		findMaximum(number1,number2,number3);
 		
-		if(string3.compareTo(maximum) > 0)
-			maximum=string3;
-		
-		System.out.println("Maximum of "+string1+", "+string2+" & "+string3+" is: "+maximum);
-		
+		String string1="adamX", string2="adam1", string3="adam2";
+		findMaximum(string1,string2,string3);
 	}
 
 }
